5822d320c08159bade62c0227ed0be965e888b59
Gilbert Luis <gilbertluis97@gmail.com>
2022-04-14 15:00:09 -0400

    comenzando crud user

--- src/components/shared/table/table.vue
+++ src/components/shared/table/table.vue
@@ -1,37 +1,114 @@
 <template>
-  <div class="icons-list">
-    <a-tooltip placement="top" v-if="props.visible_edit" title="Modificar Elemento">
-      <a-icon type="edit" style="padding-right: 10px" @click="v_instance.onEditing(object)" theme="twoTone"/>
-    </a-tooltip>
-    <a-tooltip placement="top" v-if="props.visible_view" title="Mostrar Elemento">
-      <a-icon type="eye" style="padding-right: 10px" theme="twoTone" twoToneColor="green"/>
-    </a-tooltip>
-    <a-tooltip placement="top" v-if="props.visible_delete" title="Borrar Elemento">
-      <a-icon type="delete" style="padding-right: 10px" @click="showDeleteConfirm" theme="twoTone"
-              twoToneColor="red"/>
-    </a-tooltip>
+  <div>
+  
+    <a-table
+      :row-selection="{ selectedRowKeys: state.selectedRowKeys ,onChange: onSelectChange }"
+      :columns="columns"
+      :data-source="data"
+      
+    >
+        <template #bodyCell="{ column,record }">
+      <template v-if="column.dataIndex === 'action'">
+        <a-popconfirm title="Title" @confirm="onDelete(record.id)">
+         <a-button type="danger" > <DeleteOutlined /></a-button>
+            </a-popconfirm>
+            <a-button type="primary" ><EyeOutlined /> </a-button>
+  
+      </template>
+    </template>
+    
+  </a-table>
   </div>
+    <div style="margin-bottom: 16px">
+      <a-button type="danger" :disabled="!hasSelected" :loading="state.loading" @click="deleteusers">
+       Eliminar
+      </a-button>
+      <span style="margin-left: 8px">
+        <template v-if="hasSelected">
+          {{ `Selected ${state.selectedRowKeys.length} items` }}
+        </template>
+      </span>
+    </div>
 </template>
 <script lang="ts" setup>
-import {  ref } from 'vue';
+import { defineComponent, computed, ref, onMounted, reactive } from 'vue';
+import  {DeleteOutlined} from '@ant-design/icons-vue';
+import  {EyeOutlined} from '@ant-design/icons-vue';
+import {getUsers} from '../../../service/user.service';
+import { User } from '@/types/modelTypes';
 
-interface DataType {
-  key: string | number;
-  name: string;
-  age: number;
-  address: string;
+
+ let data=ref<User[]>([]);
+
+const state = reactive<{
+      selectedRowKeys:User['id'][];
+      loading: boolean;
+    }>({
+      selectedRowKeys:[1], // Aqui configurar a columna por default
+      loading: false,
+    });
+   
+    onMounted(
+     async () => {
+          try {
+   data.value =  (await getUsers()).data;
+} catch (error) {
 }
+    })
+ 
+ 
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Correo',
+    dataIndex: 'email',
+  },
+  {
+    title: 'rol',
+    dataIndex: 'role',
+  },
+ { title: 'Operaciones',
+    dataIndex: 'action',
+  },
+];
 
-})
+
+
+
+ 
+   
+
+
+    //const selectedRowKeys = ref<User['id'][]>([]); // Check here to configure the default column
+
+    const hasSelected = computed(() => state.selectedRowKeys.length > 0);
+
+    const deleteusers = () => {
+      state.loading = true;
+      // ajax request after empty completing
+      setTimeout(() => {
+        state.loading = false;
+        state.selectedRowKeys = [];
+      }, 1000);
+    };
+    
+    const onSelectChange = (selectedRowKeys:number[]) => {
+      console.log('selectedRowKeys changed: ', selectedRowKeys);
+      
+      state.selectedRowKeys = selectedRowKeys;
+    };
+
+        const onDelete = (key: number) => {
+          console.log(data.value);
+      data.value = data.value.filter(item => item.id !== key);
+          console.log(data.value);
+    };
+
 </script>
 
-
-
-
-
-<style scoped>
-/*.delete_confirm*/
+<style lscoped>
 
 </style>
-
-
