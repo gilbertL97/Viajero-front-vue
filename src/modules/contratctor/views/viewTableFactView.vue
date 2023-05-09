36ef0019ebb2a2672ac0fb76c7b3ab946c77b4cb
Gilbert Luis <gilbertluis97@gmail.com>
2023-05-09 16:01:14 -0400

    factorizacion en la vista de factura

---
+++ src/modules/contratctor/views/viewTableFactView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="table-header"
+        ><h4 style="padding-right: 5px"> Fecha Inicio </h4>
+        <a-date-picker v-model:value="filter" picker="month" />
+    </div>
+    <TableContractorFact ref="table">
+        <DropdownExport
+            url="/contractor/invoicing/excel"
+            :filter="{ dateInvoicing }"
+            title="Cliente"
+        />
+    </TableContractorFact>
+</template>
+
+<script setup lang="ts">
+    import TableContractorFact from '../components/table/tableContractorFact.vue';
+    import DropdownExport from '@/components/shared/export/dropdownExport.vue';
+
+    const dateInvoicing = ref<string>();
+    const filter = ref<Date>();
+    const table = ref(TableContractorFact);
+    watch([filter], () => {
+        table.value.getData(filter.value);
+        dateInvoicing.value = filter.value?.toISOString();
+    });
+</script>
+
+<style scoped>
+    .table-header {
+        display: inline-flex;
+        gap: 1;
+    }
+</style>
