22e187eedf6b34f1f054d7402a0f2d57bd2d9f64
Gilbert Luis <gilbertluis97@gmail.com>
2024-02-01 16:38:05 -0500

    cambiando el /refresh al vista home

--- src/views/default/backTemp.vue
+++ src/views/default/backTemp.vue
@@ -3,7 +3,31 @@
         <img src="../../assets/imagenes/PUBLICIDINFO.jpg" alt="Publicidad info" />
     </div>
 </template>
+<script lang="ts" setup>
+    import useRefreshTokenService from '@/modules/auth/composable/useRefreshTokenService';
+    import { useRouter } from 'vue-router';
+    import { useAuthStore } from '@/modules/auth/store/auth.store.c';
+    const router = useRouter();
+    const { logout } = useAuthStore();
+    const { postRfresh, logout2 } = useRefreshTokenService();
+
+    onMounted(async () => {
+        const token = await postRfresh().catch(async () => await logout());
+        if (!token) await logoutHome();
+    });
+    const logoutHome = async () => {
+        await logout2().catch((err) => {
+            if (err.status == 403) console.log(err);
+            logout();
+        });
+        //cancelInterceptor();
+        router.push({ name: 'login' });
+    };
+</script>
 <style scoped>
+    .imagen {
+        background: url(../images/icon-cart.png) no-repeat;
+    }
     .imagen {
         width: 96%;
         margin: 0 auto;
--- src/views/home/indexHome.vue
+++ src/views/home/indexHome.vue
@@ -4,7 +4,7 @@
 
         <a-layout>
             <sider-lay />
-            <a-layout-content :loading="load">
+            <a-layout-content>
                 <router-view />
             </a-layout-content>
         </a-layout>
@@ -15,22 +15,6 @@
     import headerVue from '@/views/layouts/header/headerLay.vue';
     import footerIndex from '@/views/layouts/footer/footerLay.vue';
     import siderLay from '@/views/layouts/sidebar/sidebarLay.vue';
-    import { onMounted, ref } from 'vue';
-    import useRefreshTokenService from '@/modules/auth/composable/useRefreshTokenService';
-    import { useRouter } from 'vue-router';
-    const router = useRouter();
-    const { postRfresh, logout2 } = useRefreshTokenService();
-    const load = ref(false);
-
-    onMounted(async () => {
-        const token = await postRfresh().catch(async () => await logout());
-        if (!token) await logout();
-    });
-    const logout = async () => {
-        await logout2();
-        //cancelInterceptor();
-        router.push({ name: 'login' });
-    };
 </script>
 <style scoped>
     .ant-layout-sider {
