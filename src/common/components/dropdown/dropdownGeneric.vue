fb9427fda64b3e348c4b1bbeb099e8e02160063c
Gilbert Luis <gilbertluis97@gmail.com>
2024-03-31 03:00:30 -0400

    cambiando el componentte generico ya q no se ponia en empty

--- src/common/components/dropdown/dropdownGeneric.vue
+++ src/common/components/dropdown/dropdownGeneric.vue
@@ -1,6 +1,7 @@
 <template>
     <a-select
-        v-model:value="value"
+        v-model:value="value.value"
+        v-model:label="value.label"
         :show-search="showSearch"
         label-in-value
         :placeholder="placeholder"
@@ -19,19 +20,19 @@ import type { SelectProps } from 'ant-design-vue';
         label: string, value: string, options?: string,disabled?: string
     }
     type Select = {
-        label: string | undefined;
-        value: number | number[] | undefined;
+        label: string;
+        value: number | number[] | string;
     };
     const props = defineProps<{
         data?: any[]; //arreglo completo de datos
         showSearch?: boolean; //si se usa para buscar
         propertySearch: propToSearch; //la propiedad a buscar en los objetos
         placeholder?: string; //el placehoder
-        modelValue: any// | string | number; //el valor seleccionado
+        modelValue?: any// | string | number; //el valor seleccionado
     }>();
     const value = reactive<Select>({
-        label:undefined,
-        value:undefined,
+        label:'',
+        value:'',
     });
 //TODO terminarn el filter option
     const filterOption = (input: string, option: any) => {
@@ -47,10 +48,15 @@ import type { SelectProps } from 'ant-design-vue';
     // };
 
     watch(
-        () => props.modelValue,
-        (newVal) => {
-            value.label = newVal[props.propertySearch.label];
-            value.value = newVal[props.propertySearch.value];
+       [ () => props.modelValue,value],
+        () => {
+            if(props.modelValue){
+            value.label = props.modelValue[props.propertySearch.label];
+            value.value = props.modelValue[props.propertySearch.value];}
+            else {
+                value.label = '';
+                value.value ='';
+            }
         },
     );
     const handleChange: SelectProps['onChange'] = (value2: any) => {
