bd57e256c1fd9d88b457b8164d61dc7298b64203
Gilbert Luis <gilbertluis97@gmail.com>
2024-04-18 13:07:57 -0400

    formateando las fechas

--- src/modules/contratctor/components/table/tableContractorPolicyResume.vue
+++ src/modules/contratctor/components/table/tableContractorPolicyResume.vue
@@ -1,31 +1,38 @@
 <template>
     <a-table
         :columns="columns"
-        size="small"
+        size="middle"
         :loading="loading"
-        :data-source="data.policies"
+        :data-source="data.contractors"
     >
         <template #customFilterIcon> <slot></slot></template>
-        <template #summary>
-            <a-table-summary-row>
-                <a-table-summary-cell>Total</a-table-summary-cell>
-                <a-table-summary-cell>-</a-table-summary-cell>
-                <a-table-summary-cell>
-                    <a-typography-text type="danger">{{
+        <template #bodyCell="{ column, record }">
+                <template v-if="column.dataIndex === 'start_date'">
+                <h3>{{ dayjs(record.start_date).format('DD/MM/YYYY') }}</h3>
+            </template></template>
+        <template #summary >
+            <a-table-summary fixed>
+                <a-table-summary-row>
+                    <a-table-summary-cell>Total</a-table-summary-cell>
+                    <a-table-summary-cell>-</a-table-summary-cell>
+                    <a-table-summary-cell>
+                     <a-typography-text type="danger">{{
                         data.totalTravelers
-                    }}</a-typography-text>
-                </a-table-summary-cell>
-                <a-table-summary-cell>
-                    <a-typography-text type="danger">{{
+                        }}</a-typography-text>
+                    </a-table-summary-cell>
+                    <a-table-summary-cell>
+                        <a-typography-text type="danger">{{
                         data.totalAmount
-                    }}</a-typography-text>
-                </a-table-summary-cell>
-            </a-table-summary-row>
+                        }}</a-typography-text>
+                    </a-table-summary-cell>
+                </a-table-summary-row>
+            </a-table-summary>
         </template>
     </a-table>
 </template>
 
 <script setup lang="ts">
+import dayjs from 'dayjs';
 import { ContractorPolicyTotal } from '../../types/contractor.types';
 
 
