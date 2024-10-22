05e5de32eb4d3d9a53ef226e1084a26b0e4a41e4
Gilbert Luis <gilbertluis97@gmail.com>
2024-10-22 06:00:18 -0400

    cambiando sacando el interceptor de axios aun fichero aparte

--- src/service/useHttpMethods.ts
+++ src/service/useHttpMethods.ts
@@ -1,18 +1,19 @@
 import { Payload } from '@/common/types/generic.type';
 import API from './api';
 import { useAuthStore } from '@/modules/auth/store/auth.store.c';
-import refreshTokens from '@/modules/auth/composable/useRefreshTokenService';
+import useRefreshTokens from '@/modules/auth/composable/useRefreshTokenService';
 import { AxiosError, AxiosRequestConfig } from 'axios';
 // import { useRouter } from 'vue-router';
 import { storeToRefs } from 'pinia';
 import { addToken } from '@/intercerptors/request.interceptors/addTokenRequestInterceptor';
+import { resendRefreshTokenIfExpire } from '@/intercerptors/response.interceptors/resendRefreshToken';
 
 type AxiosRequestConfigRetry = AxiosRequestConfig & { _retry?: boolean }; //?extiendo la clase para agregarle una propiedad para saber si se reintento
 export default function useHttpMethods() {
     const { logout } = useAuthStore();
     const { acces_token } = storeToRefs(useAuthStore());
     // const router = useRouter();
-    const { postRfresh } = refreshTokens();
+    const { postRfresh } = useRefreshTokens();
     const get = (path: string, query?: any) => {
         return API.request({
             method: 'GET',
@@ -57,31 +58,14 @@ export default function useHttpMethods() {
             responseType: 'json',
         });
     };
-    // const redirectLogin = () => {
-    //     logout();
-    //     router.push({ name: 'login' });
-    // };
     API.interceptors.request.use((config) => addToken(config,acces_token.value));
     API.interceptors.response.use(
         (res) => {
             return res;
         },
-        async (error: AxiosError) => {
-            const originalConfig: AxiosRequestConfigRetry = error.config;
-            console.log(originalConfig.url?.includes('/auth/'));
-            //que no me vuelva hcer una peticion a ningun endpoin t de auth
-            if (error.response && !originalConfig.url?.includes('/auth/')) {
-                if (error.response.status == 401 && !originalConfig._retry) {
-                    originalConfig._retry = true;
-                    await postRfresh().catch(() => logout());
-                    return API(originalConfig);
-                }
-                // redirectLogin();
-            }
-            throw error;
-        },
+        async (error: AxiosError)=> resendRefreshTokenIfExpire(error)
     );
-
+  
     return {
         get,
         patch,
